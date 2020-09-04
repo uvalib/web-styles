4a68c57626f90782602535e929eb61a30294acd1
Doug Chestnut <dougchestnut@gmail.com>
2020-09-04 06:34:25 -0400

    test for fa icons before loading

--- src/icons.js
+++ src/icons.js
@@ -1,4 +1,19 @@
-var link = document.createElement('link');
-link.rel = "stylesheet";
-link.href = "https://use.fontawesome.com/releases/v5.14.0/css/all.css";
-document.head.appendChild(link);
+(function () {
+  let span = document.createElement('span');
+
+  span.className = 'fa';
+  span.style.display = 'none';
+  document.body.insertBefore(span, document.body.firstChild);
+
+  function css(element, property) {
+    return window.getComputedStyle(element, null).getPropertyValue(property);
+  }
+
+  if (css(span, 'font-family') !== 'FontAwesome') {
+    var link = document.createElement('link');
+    link.rel = "stylesheet";
+    link.href = "https://use.fontawesome.com/releases/v5.14.0/css/all.css";
+    document.head.appendChild(link);
+  }
+  document.body.removeChild(span);
+})();
