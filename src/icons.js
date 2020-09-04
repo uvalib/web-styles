26b87fa0e9ca45483963a84b8cd6adbc81b6ec33
Doug Chestnut <dougchestnut@gmail.com>
2020-09-04 06:57:16 -0400

    fa loading

--- src/icons.js
+++ src/icons.js
@@ -1,19 +1,6 @@
 (function () {
-  let span = document.createElement('span');
-
-  span.className = 'fa';
-  span.style.display = 'none';
-  document.body.insertBefore(span, document.body.firstChild);
-
-  function css(element, property) {
-    return window.getComputedStyle(element, null).getPropertyValue(property);
-  }
-
-  if (css(span, 'font-family') !== 'FontAwesome') {
-    var link = document.createElement('link');
-    link.rel = "stylesheet";
-    link.href = "https://use.fontawesome.com/releases/v5.14.0/css/all.css";
-    document.head.appendChild(link);
-  }
-  document.body.removeChild(span);
+  var link = document.createElement('link');
+  link.rel = "stylesheet";
+  link.href = "https://use.fontawesome.com/releases/v5.14.0/css/all.css";
+  document.head.appendChild(link);
 })();
